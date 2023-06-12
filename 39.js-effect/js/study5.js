f6a02d55f6c20ac2f7f59397a38215574b2d2597
subin9898 <yewoo28@gamil.com>
2023-06-12 18:17:35 +0900

    ghgh

--- 39.js-effect/js/study5.js
+++ 39.js-effect/js/study5.js
@@ -2,6 +2,55 @@ $(document).ready(function () {
 
 
   let count = 0;
+
+
+  let perView;
+  //train클래스 너비를 변경 -> train클래스의 너비를 불러와서 perView로 나눈 후 결과를 다시 적용
+  let stationWidth;
+  let trainWidth;
+  // $(".train").width(trainWidth)
+
+  let winWidth = $(window).width()
+  // console.log(winWidth)
+
+  if (winWidth > 1024) {
+    perView = 2.5
+    stationWidth = $(".station").width()
+    trainWidth = stationWidth * 5 / perView
+    $(".train").width(trainWidth)
+  } else if (winWidth >= 768) {
+    perView = 2
+    stationWidth = $(".station").width()
+    trainWidth = stationWidth * 5 / perView
+
+  } else {
+
+    perView = 1
+    stationWidth = $(".station").width()
+    trainWidth = stationWidth * 5 / perView
+  }
+
+  $(window).resize(function () {
+    let winWidth = $(window).width()
+    // console.log(winWidth)
+
+    if (winWidth > 1024) {
+      perView = 2.5
+      stationWidth = $(".station").width()
+      trainWidth = stationWidth * 5 / perView
+      $(".train").width(trainWidth)
+    } else if (winWidth >= 768) {
+      perView = 2
+      stationWidth = $(".station").width()
+      trainWidth = stationWidth * 5 / perView
+
+    } else {
+
+      perView = 1
+      stationWidth = $(".station").width()
+      trainWidth = stationWidth * 5 / perView
+    }
+  })
   $(".btnNext").click(function (e) {
     count++
     e.preventDefault()
