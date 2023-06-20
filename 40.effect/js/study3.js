34a4c2e9e723362a1ac15d5d30688140590591c6
subin9898 <yewoo28@gamil.com>
2023-06-20 17:04:44 +0900

    erer

--- 40.effect/js/study3.js
+++ 40.effect/js/study3.js
@@ -1,17 +1,21 @@
 $(document).ready(function () {
 
 
+  let a = [1, 2, 3, "hi"]
+
 
   $(".btn1").click(function () {
     textAni(".title", {
       // aniDelay: 0.5,
       // aniSpeed: 1,
-      textSize: 80
+      // textSize: 80
     })
   })
-  // textAni(".title", {
-  //   textsize: 100
-  // })
+  textAni(".title", {
+    textsize: 100,
+    aniSpeed: 0.5,
+    effect: "moveBottomToTop"
+  })
 })
 
 
