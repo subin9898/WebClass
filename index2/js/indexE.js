632cca29ae47948d8bfdd6b72f73d62f4d03b86c
Subin Park <yewoo28@gmail.com>
2023-06-25 16:55:34 +0900

    dfdf

--- index2/js/indexE.js
+++ index2/js/indexE.js
@@ -1,11 +1,14 @@
 $(document).ready(function () {
 
   let count = 0;
+  let station = $(".station").width()
 
   setInterval(function () {
     count++
     if (count > 2) { count = 0 }
-    $(".train").css("transform", "translateX(" + "calc()+)")
-}, 2500)
+    const translate = -station *count
+    $(".train").css("transform", "translateX(" + translate + "px)");
+  }, 2500);
+
 
 })
