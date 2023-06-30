87c624711d92aa6bc05b3c9f50c25da12a6d257f
subin9898 <yewoo28@gamil.com>
2023-06-30 17:52:54 +0900

    erer

--- 41.svg/js/study5.js
+++ 41.svg/js/study5.js
@@ -23,9 +23,16 @@ $(document).ready(function () {
       $(".movingArea").css("position", "fixed")
       $(".movingArea").css("left", "0")
       $(".movingArea").css("top", "0")
-      let percent = (scrollDistance / (8000 - movingAreaHeight) * 100)
+
+
+      let percent = (scrollDistance / (8000 - movingAreaHeight) * 100)  //0~100까지의 수
       $("#progress path").css("stroke-dashoffset", pathLength - ((pathLength * percent) / 100))
       $(".per").text(Math.round(percent) + "%")
+
+      let percent2 = (scrollDistance / (8000 - movingAreaHeight) * 87.5)  //0~87.5까지의 수
+      $(".train").css("transform", "translateX(-" + percent2 + "%)")
+
+
 
     }
     if (scrollDistance > 8000 - movingAreaHeight) {
